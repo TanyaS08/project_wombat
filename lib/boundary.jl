dbd3602fd046c7a329d2b96a6f35e3dfc129c76e
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-06 14:27:20 +0200

    type change

--- lib/boundary.jl
+++ lib/boundary.jl
@@ -5,7 +5,7 @@ Extracts candidate boundaries using calculated rates of change (M) on specified
 threshold. Default threshold is 10%, meaning that the top 10% of pixels are
 selected as part of the boundaries.
 """
-function boundaries(M::Matrix{T}; threshold::T=0.1) where {T <: Number}
+function boundaries(M::Matrix{Float32}; threshold::T=0.1) where {T <: Number}
     limit = floor(Int, size(M, 2) * size(M, 1) * threshold)
     M_n = denserank(M, rev=true) # ranks largest to smallest
     return M_n .< limit
